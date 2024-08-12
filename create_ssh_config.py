1590ce48cebbf57dd71d2a72cb4b40a0b2438799
prudhviponnada <121768826+prudhviponnada@users.noreply.github.com>
2024-08-12 21:26:07 +0200

    Update create_ssh_config.py

--- create_ssh_config.py
+++ create_ssh_config.py
@@ -2,54 +2,75 @@ import os
 import json
 import sys
 
-def create_ssh_config_file(tag, instances):
+def write_hosts(tag, instances):
+    with open('./hosts', 'w') as f:
+        f.write('[bastion]\n')
+        hostname = tag + '_bastion'
+        f.write(f'{hostname}\n')
+        f.write('[HAproxy]\n')
+        hostname = tag + '_proxy1'
+        f.write(f'{hostname}\n')
+        hostname = tag + '_proxy2'
+        f.write(f'{hostname}\n')
+        f.write('\n')
+        f.write('[primary_proxy]\n')
+        hostname = tag + '_proxy1'
+        f.write(f'{hostname}\n')
+        f.write('[backup_proxy]\n')
+        hostname = tag + '_proxy2'
+        f.write(f'{hostname}\n')
+        f.write('\n')
+        
+        f.write('[dev]\n')
+        for _, details in instances.items():
+            if details['name'] not in [f"{tag}_bastion", f"{tag}_proxy1", f"{tag}_proxy2"]:
+                f.write(f"{details['name']}\n")
+        f.write('\n')
+        f.write('[all:vars]\n')
+        f.write('ansible_user=ubuntu\n')
+
+
+def create_ssh_config_file(tag, instances_file_path, private_key_path):
+    # Load instances data from the file
+    with open(instances_file_path, 'r') as file:
+        instances = json.load(file)
+
     print(f"Tag: {tag}")
     print(f"Instances: {instances}")
 
-    ssh_config_path = os.path.expanduser(f"~/.ssh/{tag}_config")
+    ssh_config_path = os.path.expanduser(f"./{tag}_config")
     with open(ssh_config_path, "w") as file:
         file.write("# SSH Configurations\n")
         file.write("Host *\n")
         file.write(f"\tUser ubuntu\n")
-        file.write(f"\tIdentityFile ~/.ssh/{tag}_private.pem\n")
+        file.write(f"\tIdentityFile {private_key_path}\n")
         file.write(f"\tStrictHostKeyChecking no\n")
         file.write(f"\tPasswordAuthentication no\n")
-        file.write(f"\tServerAliveInterval 60\n\n")
-
         # Additional settings for all hosts
         file.write(f"\tForwardAgent yes\n")
-        file.write(f"\tControlMaster auto\n")
-        file.write(f"\tControlPath ~/.ssh/ansible-%r@%h:%p\n")
-        file.write(f"\tControlPersist yes\n\n")
 
         # Loop through the instances and configure each one
         for name, details in instances.items():
-            if details["floating_ip"]:
+            
+            if name == "bastion":
                 file.write(f"Host {tag}_{name}\n")
                 file.write(f"\tHostName {details['floating_ip']}\n")
-                if name.startswith("proxy"):
-                    file.write(f"\tProxyCommand ssh -W %h:%p {tag}_bastion\n")
+            
+                
             else:
                 file.write(f"Host {tag}_{name}\n")
                 file.write(f"\tHostName {details['internal_ip']}\n")
-                file.write(f"\tProxyCommand ssh -W %h:%p {tag}_bastion\n")
+                file.write(f"\tproxyjump {tag}_bastion\n")
             file.write("\n")
 
     print(f"SSH config file generated: {ssh_config_path}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python create_ssh_config.py <tag> <instances>")
+        print("Usage: python create_ssh_config.py <tag> <instances-file>")
         sys.exit(1)
     
     tag = sys.argv[1]
-    instances = {}
-    
-    # Assuming instances is passed as a JSON string for simplicity
-    try:
-        instances = json.loads(sys.argv[2])
-    except json.JSONDecodeError:
-        print("Invalid instances JSON format")
-        sys.exit(1)
+    instances_file_path = sys.argv[2]
 
-    create_ssh_config_file(tag, instances)
+    create_ssh_config_file(tag, instances_file_path, sys.argv[3])
